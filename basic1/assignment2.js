8f26eb8904076b05ed1226b67a5433c27de9cd97
Sarthak36-s <sarthaksrivastava928@gmail.com>
2025-12-09 17:37:36 +0000

    workd2

---
+++ basic1/assignment2.js
@@ -0,0 +1,21 @@
+let a=5
+let b=4
+let c=a+b
+console.log(c)
+console.log("Hello"+"Sarthak")
+if(a%2==0)
+{
+    console.log("Even")
+}
+else
+    console.log("Odd")
+let t=34
+let f=(t*9/5)+32
+console.log(f+"F")
+
+if(a>b&&a>c)
+    console.log(a)
+else if(b>a&&b>c)
+    console.log(b)
+else
+    console.log(c)
---
+++ basic1/day2.js
@@ -0,0 +1,32 @@
+//let a=23
+//const b="abc"
+//var c=78
+//d=88
+//console.log(a);
+//console.log(b);
+//console.log(c);
+//console.log(d);
+
+/*let a="abc"
+let b=""
+let d=null
+let e=true
+console.log(a);
+console.log(b);
+console.log(d);
+console.log(e);
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+*/
+
+let score=33;
+console.log(typeof score);
+console.log(typeof (score));
+
+let newscroe=Number(score);
+console.log(typeof newscore);
+console.log(typeof (newscorescore));
+
