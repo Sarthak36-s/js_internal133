75909ac24833fb3f95ecff2af21b9a183a339e67
Sarthak36-s <sarthaksrivastava928@gmail.com>
2025-12-09 05:29:36 +0000

    day2task

---
+++ .devcontainer/basic/day2task.js
@@ -0,0 +1,8 @@
+let a=23
+const b="abc"
+var c=78
+d=88
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
---
+++ basic1/d2.js
@@ -0,0 +1,8 @@
+let score=33;
+console.log(typeof score);
+console.log(typeof (score));
+
+let newscroe=Number(score);
+console.log(typeof newscore);
+console.log(typeof (newscorescore));
+
