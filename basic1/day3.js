c2bacc9938c2d3c1c165d83353924f12b151519e
Sarthak36-s <sarthaksrivastava928@gmail.com>
2025-12-10 05:29:18 +0000

    d3

---
+++ basic1/day3.js
@@ -0,0 +1,29 @@
+function fun(n)
+{
+  return n*2;
+}
+ let ch=fun(72);
+ 
+
+ let arr=[1,2,3,4,5,"apple","banana",ch];
+ //console.log(arr);
+ let fruit=["apple","mango","banana","peach","orange"];
+ //fruit.push("Papaya")
+ //console.log(fruit);
+// fruit.pop("orange");
+  //console.log(fruit);
+ // fruit.unshift("apple");
+   //console.log(fruit);
+   //fruit.shift("mango");
+    //console.log(fruit);
+    //let num=[1,3,4];
+   // let dou=num.filter(n=>n>3);
+   /*let nums=[2,4,6,8];
+    let dou=nums.map(n=>n*2);
+    console.log(dou);
+    let num1=[10,25,30,5,60];
+    let tot=num1.filter(n=>n>20);
+    console.log(tot);
+    let num2=[1,2,3,4,5];
+    let tot1=num2.reduce((acc,val)=>acc+val,0);
+    console.log(tot1);*/
